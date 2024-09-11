97132b2af84658e6f951f1b2c0fd03b67e3241a1
Dylan Bielser <djb-123@hotmail.com>
2024-09-11 00:25:52 -0600

    social icons added

--- components/ui/HeroSection.tsx
+++ components/ui/HeroSection.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button } from "@/components/ui/button"; // Update this import path as needed
+import { FaInstagram, FaFacebook, FaSpotify, FaApple } from "react-icons/fa";
 
 const HeroSection = () => {
   return (
@@ -18,7 +19,7 @@ const HeroSection = () => {
           <p className="text-xl md:text-2xl mb-8 text-white">
             Raw Lyricism | Unique Flow | Genre Versatility
           </p>
-          {/* Flex Column for Button and Music Embed */}
+          {/* Flex Column for Button, Social Icons, and Music Embed */}
           <div className="flex flex-col items-center justify-center gap-4">
             <Button
               size="lg"
@@ -30,6 +31,38 @@ const HeroSection = () => {
             >
               Tickets
             </Button>
+
+            {/* Social Media Icons */}
+            <div className="flex space-x-4 text-white mt-4">
+              <a
+                href="https://www.instagram.com/pharaohthekid99/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaInstagram className="w-6 h-6 hover:text-gray-400" />
+              </a>
+              <a
+                href="https://www.facebook.com/p/Pharaoh-The-Kid-100027352491875/?paipv=0&eav=AfaK_c1Qv8b1ZvMARUFd9AAhlhTa_MEuGJxbQjX23CV6L40LOHEwuJwsx5XV2zrTGnU&_rdr"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaFacebook className="w-6 h-6 hover:text-gray-400" />
+              </a>
+              <a
+                href="https://open.spotify.com/artist/37M8wx1NvoiQujq2cN0hhT"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaSpotify className="w-6 h-6 hover:text-gray-400" />
+              </a>
+              <a
+                href="https://music.apple.com/us/artist/pharaoh-the-kid/1524193407"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaApple className="w-6 h-6 hover:text-gray-400" />
+              </a>
+            </div>
 
             <iframe
               allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
