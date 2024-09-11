4bfadaf215a99a889f062aa7a2552a95ec30ce2d
Dylan Bielser <djb-123@hotmail.com>
2024-09-11 00:28:34 -0600

    social links added

--- components/ui/ContactSection.tsx
+++ components/ui/ContactSection.tsx
@@ -1,17 +1,46 @@
 import React from "react";
-import { Mail, Phone } from "lucide-react";
+import { FaInstagram, FaFacebook, FaSpotify, FaApple } from "react-icons/fa";
 
 const ContactSection = () => {
   return (
-    <div>
-      <h2 className="text-4xl font-bold mb-6 text-white">Get In Touch</h2>
-      <div className="space-y-4">
-        <p className="flex items-center text-white">
-          <Phone className="mr-3" /> (319) 759-1641
-        </p>
-        <p className="flex items-center text-white">
-          <Mail className="mr-3" /> pharaohthekid99@gmail.com
-        </p>
+    <div className="text-white">
+      <h2 className="text-3xl font-bold mb-4">Contact Pharaoh The Kid</h2>
+      <p className="mb-4">
+        For inquiries or bookings, please reach out via the form below or follow
+        on social media.
+      </p>
+      {/* Add contact form or contact details here */}
+
+      {/* Social Media Icons */}
+      <div className="flex space-x-4 text-white mt-8">
+        <a
+          href="https://www.instagram.com/pharaohthekid99/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaInstagram className="w-6 h-6 hover:text-gray-400" />
+        </a>
+        <a
+          href="https://www.facebook.com/p/Pharaoh-The-Kid-100027352491875/?paipv=0&eav=AfaK_c1Qv8b1ZvMARUFd9AAhlhTa_MEuGJxbQjX23CV6L40LOHEwuJwsx5XV2zrTGnU&_rdr"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaFacebook className="w-6 h-6 hover:text-gray-400" />
+        </a>
+        <a
+          href="https://open.spotify.com/artist/37M8wx1NvoiQujq2cN0hhT"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaSpotify className="w-6 h-6 hover:text-gray-400" />
+        </a>
+        <a
+          href="https://music.apple.com/us/artist/pharaoh-the-kid/1524193407"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaApple className="w-6 h-6 hover:text-gray-400" />
+        </a>
       </div>
     </div>
   );
